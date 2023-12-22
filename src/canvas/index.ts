41f0e4be3c3b4ec50d5756014f818cdb202fbfd0
Efra <efrainschettini.123@gmail.com>
2023-12-22 00:52:36 -0300

    msg

--- src/canvas/index.ts
+++ src/canvas/index.ts
@@ -3,6 +3,6 @@ import { createCanvas } from "canvas"
 const canvas = createCanvas(800, 600)
 const ctx = canvas.getContext("2d")
 
-console.log("created canvas")
+console.log("Canvas created")
 
 export { canvas, ctx }
